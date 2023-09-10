c40241a384c0de9f6f306b4cfdc7cd1b8e3884ec
Thomas Vermeer <THQgameworld@gmail.com>
2023-09-10 09:21:08 +0200

    start header styling logo + quote +background

--- header.php
+++ header.php
@@ -1,7 +1,14 @@
 <header>
+    <div class="main-items-header">
+        <div class="logo">
+            <img src="img/gaming_image.png" alt="gaming">
+            <h1>spelletjes-website</h1>
 
-
-
+        </div>
+        <div class="quote">
+            <h1>where gaming never stops</h1>
+        </div>
+    </div>
 
 
 
